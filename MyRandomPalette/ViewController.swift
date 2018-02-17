b7f563e95a003d36920c392fff137e0d6ec1932c
Ong Xin Ying <ongxinying@ongs-macbook-pro.local>
2018-02-17 23:21:46 +0800

    Initial Commit

---
+++ MyRandomPalette/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyRandomPalette
+//
+//  Created by Ong Xin Ying on 17/2/18.
+//  Copyright © 2018 Ong Xin Ying. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
